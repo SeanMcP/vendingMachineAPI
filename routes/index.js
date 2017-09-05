251435369cf8ba8852501510df69a2fe3bbe931b
Sean McPherson <seanmcp25@gmail.com>
2017-09-05 15:13:16 -0400

    Clean up files

--- routes/index.js
+++ routes/index.js
@@ -12,13 +12,6 @@ const sendMessage = function(status, data) {
   return obj
 }
 
-// GET /api/customer/items - get a list of items
-// POST /api/customer/items/:itemId/purchases - purchase an item
-// GET /api/vendor/purchases - get a list of all purchases with their item and date/time
-// GET /api/vendor/money - get a total amount of money accepted by the machine
-// POST /api/vendor/items - add a new item not previously existing in the machine
-// PUT /api/vendor/items/:itemId - update item quantity, description, and cost
-
 router.get('/api/customer/items', function(req, res) {
   models.Item.findAll()
   .then(function(data) {
